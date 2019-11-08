e65f4340cbb3d489777a8f4719a178b9ddc6a72b
ephur <richard.maynard@gmail.com>
2019-11-08 01:50:38 -0600

    add alias for easy NFS mounting

--- aliases.zsh
+++ aliases.zsh
@@ -14,6 +14,7 @@ alias grep='grep --color=auto'
 alias neofetch='neofetch --config ~/.nf.conf'
 alias neofetchu='neofetch --config ~/.nf.conf -su'
 alias i='or-infra'
+alias nfs='for i in /nfs/*; do mount $i; done'
 
 # Extra handy things
 alias resrc="source ~/.zshrc"
